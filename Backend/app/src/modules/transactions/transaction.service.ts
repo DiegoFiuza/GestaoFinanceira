2cf5869e8b20fa10079990da451d83b67c67dd9e
DiegoFiuza <nasserdiegof@gmail.com>
2026-02-06 17:31:00 -0300

    Feat:Finalizado transactions services

--- Backend/app/src/modules/transactions/transaction.service.ts
+++ Backend/app/src/modules/transactions/transaction.service.ts
@@ -5,7 +5,7 @@ import {
 } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Transaction, TransactionDocument } from './entities/entity';
-import { Model } from 'mongoose';
+import { Model, Types } from 'mongoose';
 import { CreateTransactionDto } from './dto/create-transaction-dto';
 
 @Injectable()
@@ -50,7 +50,16 @@ export class TransactionService {
     };
   }
 
-  async findALl(id: string, year: number, month: number) {
+  async findById(id: string): Promise<TransactionDocument | null> {
+    //verifico se a string tem formato válido
+    if (!Types.ObjectId.isValid(id)) {
+      return null;
+    }
+    //garanto que vai retornar uma promisse e resolver para null ou nao
+    return this.transactionModel.findById(id).exec();
+  }
+
+  async findAllMonth(id: string, year: number, month: number) {
     const startDate = new Date(year, month - 1, 1);
     const endDate = new Date(year, month, 0, 23, 59, 59);
     return this.transactionModel
@@ -64,6 +73,38 @@ export class TransactionService {
       .exec();
   }
 
+  async calcMonth(id: string, year: number, month: number) {
+    const startDate = new Date(year, month - 1, 1);
+    const endDate = new Date(year, month, 0, 23, 59, 59);
+    let sum = 0;
+    const transactions = await this.transactionModel
+      .find({
+        user: id,
+        createdAt: {
+          $gte: startDate, // Greater than or equal (Maior ou igual)
+          $lte: endDate, // Less than or equal (Menor ou igual)
+        },
+      })
+      .exec();
+    for (const sumTransaction of transactions) {
+      if (
+        sumTransaction.type === 'expensive' ||
+        sumTransaction.type === 'fixed expensive'
+      ) {
+        const intTransaction = Number(sumTransaction.amount);
+        if (!isNaN(intTransaction)) {
+          sum -= intTransaction;
+        }
+      } else if (sumTransaction.type === 'income') {
+        const intTransaction = Number(sumTransaction.amount);
+        if (!isNaN(intTransaction)) {
+          sum += intTransaction;
+        }
+      }
+    }
+    return sum;
+  }
+
   async findFixedByDay(userId: string, day: number): Promise<Transaction[]> {
     // O MongoDB já retorna apenas as despesas do dia exato (ex: dia 10)
     return this.transactionModel
@@ -72,5 +113,31 @@ export class TransactionService {
         recurrencyDay: day,
       })
       .exec();
+  }
+
+  async findAllTransaction(userId: string): Promise<Transaction[]> {
+    return this.transactionModel.find({ user: userId }).exec();
+  }
+
+  async calcAll(id: string) {
+    let sum = 0;
+    const transactions = await this.findAllTransaction(id);
+    for (const sumTransaction of transactions) {
+      if (
+        sumTransaction.type === 'expensive' ||
+        sumTransaction.type === 'fixed expensive'
+      ) {
+        const intTransaction = Number(sumTransaction.amount);
+        if (!isNaN(intTransaction)) {
+          sum -= intTransaction;
+        }
+      } else if (sumTransaction.type === 'income') {
+        const intTransaction = Number(sumTransaction.amount);
+        if (!isNaN(intTransaction)) {
+          sum += intTransaction;
+        }
+      }
+    }
+    return sum;
   }
 }
